8a60acb513f0400638e02e8bc06a090b043423f4
Aarav Yadav <aarav25803691@gmail.com>
2025-11-13 22:51:38 -0600

    Finished move & started rigidBody struct

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
 "void main() {\n"
 "\tFragColor = color;\n"
 "}\0";
-
+int jump = 2;
 void framebufferResize(GLFWwindow* win, int width, int height);
 void framebufferResize(GLFWwindow* win, int width, int height) {
   glViewport(0, 0, width, height);
@@ -26,7 +26,11 @@ void framebufferResize(GLFWwindow* win, int width, int height) {
 
 glm::vec2 processMovement(GLFWwindow *window) {
   glm::vec2 dir(0.0f);
-  if (glfwGetKey(window, GLFW_KEY_W) || glfwGetKey(window, GLFW_KEY_UP) || glfwGetKey(window, GLFW_KEY_SPACE)) dir.y += 0.1f;
+  if (glfwGetKey(window, GLFW_KEY_W) || glfwGetKey(window, GLFW_KEY_UP) || glfwGetKey(window, GLFW_KEY_SPACE)) {
+    if (jump > 0) {
+      dir.y += 0.1f;
+    }
+  }
   // if (glfwGetKey(window, GLFW_KEY_S) || glfwGetKey(window, GLFW_KEY_DOWN)) dir.y -= 0.01f;
   if (glfwGetKey(window, GLFW_KEY_A) || glfwGetKey(window, GLFW_KEY_LEFT)) dir.x -= 1.0f;
   if (glfwGetKey(window, GLFW_KEY_D) || glfwGetKey(window, GLFW_KEY_RIGHT)) dir.x += 1.0f;
@@ -38,14 +42,29 @@ void processInput(GLFWwindow *window) {
 }
 
 void draw(GLFWwindow *window, unsigned int VAO, unsigned int VBO, unsigned int shaderProgram, const float *verts, glm::vec2 pos, glm::vec4 color, int transLoc, int colorLoc, int low, int high) {
+  if (colorLoc == -1 || transLoc == -1) std::cout << "Awh man\n";
   glBindVertexArray(VAO);
   glUseProgram(shaderProgram);
   glm::mat4 transform = glm::translate(glm::mat4(1.0f), glm::vec3(pos, 0.0f));
   glUniformMatrix4fv(transLoc, 1, GL_FALSE, glm::value_ptr(transform));
   glUniform4fv(colorLoc, 1, glm::value_ptr(color));
-  glDrawArrays(GL_TRIANGLES, low, high);
+  glDrawArrays(GL_TRIANGLES, low, high); // std::cout << "Drawn! Low: " << low << " High: " << high << '\n';
+  if (colorLoc == -1) std::cout << "COLORLOC FAILED TO INIT";
+  if (transLoc == -1) std::cout << "TRANSLOC FAILED TO INIT";
 }
 
+struct rigidBody { // Defining rigidBodies so that collision logic isn't retyped
+  glm::vec2 pos, halfSize, color;
+  float yVel = 0.0f, speed = 0.0f;
+  bool stat; // static is a C++ keyword, can't use that
+
+  void touching(rigidBody& other) {
+    if (other.pos.y + other.halfSize.y > pos.y && other.pos.y - other.halfSize.y < pos.y) { // Checks to see if player's on the same y level as the object
+      if (other.pos.x + other.halfSize.x > pos.x - halfSize.x) other.pos.x -= std::abs(halfSize.x - other.halfSize.x);
+      if (other.pos.x - other.halfSize.x > pos.x + halfSize.x) other.pos.x += std::abs(halfSize.x - other.halfSize.x);
+    }
+  }
+};
 
 int main() {
   // We're initializing GLFW here to make sure that the correct version is installed
@@ -74,7 +93,15 @@ int main() {
 
     0.05f, -0.1f, 0.0f,
     0.05f, 0.1f, 0.0f,
-    -0.05f, 0.1f, 0.0f
+    -0.05f, 0.1f, 0.0f,
+    // Middle rect thing
+    -1, -0.01, 0.0,
+    -1, 0.01, 0.0,
+    1, 0.01, 0.0, 
+
+    1, 0.01, 0.0,
+    1, -0.01, 0.0,
+    -1, -0.01, 0.0
   }; // Making verticies with (x, y, z) coords
      // The domain is [-1, 1], and the range is also [-1, 1]
   unsigned int VAO; // The V(ertex)A(rray)O(bject) stores vertex info for late use
@@ -106,14 +133,11 @@ int main() {
   float speed = 0.0f, yVel = 0.0f, grav = -0.01981f, timeValue = glfwGetTime(), greenValue = ((sin(timeValue) / 2.0f) + 0.5f);
   glm::vec2 pos(0.0f, -0.95f);
   glm::vec4 color(0.0f, 0.0f, 0.0f, 1.0f);
-  int colorLoc = glGetUniformLocation(shaderProgram, "color"), vertexColorLocation = glGetUniformLocation(shaderProgram, "color");
-  glUniform4fv(colorLoc, 1, glm::value_ptr(color));
-  glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+  int colorLoc = glGetUniformLocation(shaderProgram, "color");
+  glUniform4f(colorLoc, 0.0f, greenValue, 0.0f, 1.0f);
   while (!glfwWindowShouldClose(window)) { // Loop :D
     timeValue = glfwGetTime();
     greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-    vertexColorLocation = glGetUniformLocation(shaderProgram, "color");
-    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glm::vec2 input = processMovement(window);
@@ -122,7 +146,10 @@ int main() {
     pos.x += input.x * 0.01 + speed;
     yVel += grav * dTime;
     float limit = 1.0f, halfSize = 0.05f;
-    if (pos.y >= -0.95 + halfSize + 0.0001) pos.y += 0.01 + yVel;
+    if (pos.y >= -0.95 + halfSize + 0.0001) {
+      pos.y += 0.01 + yVel;
+      jump--;
+    }
     else {
       yVel += input.y * 0.03;
       pos.y += yVel;
@@ -131,20 +158,16 @@ int main() {
     if (pos.y < -0.95 + halfSize) {
       pos.y = -0.95 + halfSize;
       yVel = 0.0f;
+      jump = 2;
     }
     pos.x = glm::clamp(pos.x, -limit + halfSize, limit - halfSize);
-    transform = glm::translate(glm::mat4(1.0f), glm::vec3(pos, 0.0f));
-    glUseProgram(shaderProgram);
-    glUniformMatrix4fv(transLoc, 1, GL_FALSE, glm::value_ptr(transform));
-    glBindVertexArray(VAO);
     color.x = 0.5f;
     color.y = greenValue;
-    glUniform4fv(colorLoc, 1, glm::value_ptr(color));
     draw(window, VAO, VBO, shaderProgram, verts, pos, color, transLoc, colorLoc, 0, 3);
     color.y = 0.0f;
     color.x = greenValue;
-    glUniform4fv(colorLoc, 1, glm::value_ptr(color));
-    draw(window, VAO, VBO, shaderProgram, verts, pos, color, transLoc, colorLoc, 3, 6);
+    draw(window, VAO, VBO, shaderProgram, verts, pos, color, transLoc, colorLoc, 3, 3);
+    draw(window, VAO, VBO, shaderProgram, verts, glm::vec2(0.0f, -0.5f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), transLoc, colorLoc, 6, 6); // ERROR: This rectangle won't render
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
